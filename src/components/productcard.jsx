39c379787d2281020d946648d8206b6ae6e18945
Rohit <rohithrajka524@gmail.com>
2025-02-14 09:41:30 +0530

    Completed the assignment

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,37 @@
-// write product card here
+import React from 'react';
+import ViewProductButton from './button';
+
+const Productcard = () => {
+    const productImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzHt9Uak7LvbCOe1u3rMm3X9unQm1HDNziog&s';
+    const productName = 'PUMA PALERMO SNEAKERS';
+    const price = '$99.99';
+
+    const cardStyle = {
+        border: '1px solid #ccc',
+        backgroundColor: '#f9f9f9',
+        padding: '16px',
+        color: '#333',
+        borderRadius: '8px',
+        textAlign: 'center',
+        width: '200px',
+        margin: '20px auto'
+    };
+
+    const imgStyle = {
+        width: '100%',
+        height: 'auto',
+        borderRadius: '8px',
+        marginBottom: '16px'
+    };
+
+    return (
+        <div className="product-card" style={cardStyle}>
+            <img src={productImage} alt={productName} style={imgStyle} />
+            <h2>{productName}</h2>
+            <p>{price}</p>
+            <ViewProductButton />
+        </div>
+    );
+};
+
+export default Productcard;
